8f6923441a315f5f50615c69056a656c97c63466
Ali Nugen <alexandra.nugen@gmail.com>
2022-07-13 21:34:01 -0700

    updating to include print to page functions for recipe results

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+debugger
+
 var red = "#FF220C";
 var yellow ="#FFBA08"
 var accentRed = "#69140E";
@@ -28,8 +30,7 @@ var formSubmitHandler = function (event) {
     if (ingredient) { console.log('if(ingredient)')
         getRecipe(ingredient);
 
-        // repoContainerEl.textContent = '';
-        // recipeInputEl.value = '';
+
     } else {
         //change to modal
         alert('Please enter at least one ingredient! Dig to the back of your pantry!');
@@ -45,8 +46,17 @@ var getRecipe = function (ingredient) {
         return response.json()
     })
     .then(function (data) {
-        console.log(data);
+        console.log(data)
+        displayRecipe(data.hits);
+    })
+    .catch(function (error) {
+        alert('Unable to connect to Edamam.');
+        console.log(error);
     });
 };
+
+var displayRecipe = function (data) {
+    // IF ==> FOR LOOP
+    }
 
 ingredientSearchButton.addEventListener('click', formSubmitHandler);
