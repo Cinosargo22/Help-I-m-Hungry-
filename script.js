d537bb0da52dc28c9d843a9d094ece21c7d2899f
musaliyah <asmusaliar@gmail.com>
2022-07-16 10:35:10 -0700

    giphy api connected

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var ingredientInputEl = document.querySelector('.ingredient-input');
 var ingredientSearchButton = document.querySelector('.ingredient-button');
 requestUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=94c55930&app_key=%201c3476c6fc8d3b7ee88d9f887c63fd85'
 
+
 // fetch(requestUrl)
 //   .then(function (response) {
 //     return response.json();
@@ -27,6 +28,7 @@ var formSubmitHandler = function (event) {
 
     if (ingredient) { console.log('if(ingredient)')
         getRecipe(ingredient);
+        sendApiRequest(ingredient);
 
         // repoContainerEl.textContent = '';
         // recipeInputEl.value = '';
@@ -34,6 +36,7 @@ var formSubmitHandler = function (event) {
         //change to modal
         alert('Please enter at least one ingredient! Dig to the back of your pantry!');
         console.log(ingredient);
+        
     }
 };
 
@@ -50,3 +53,24 @@ var getRecipe = function (ingredient) {
 };
 
 ingredientSearchButton.addEventListener('click', formSubmitHandler);
+
+function sendApiRequest (ingredient){
+
+
+    var giphyApiKey = "C3ZPqXLVWSHKBdEwxjhZDGcmnfeuQ8LE"
+    var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=C3ZPqXLVWSHKBdEwxjhZDGcmnfeuQ8LE&q=' + ingredient + '&limit=25&offset=0&rating=g&lang=en'
+
+    fetch(giphyApiUrl).then(function(data){
+        return data.json()
+        console.log(data.json())
+    })
+    .then(function(data){
+        console.log(data)
+        console.log(data.data[0].images.fixed_height.url)
+        var imgPath = data.data[0].images.fixed_height.url
+        var img = document.createElement("img")
+        img.setAttribute("src", imgPath)
+        document.body.appendChild(img)
+
+    })
+}
